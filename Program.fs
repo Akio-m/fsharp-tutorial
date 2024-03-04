c47716ee23ee52d45474e0968ac435e56028597c
akio-m <ahigachika@gmail.com>
2024-03-04 11:33:31 +0900

    initial

---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿open MyFirstScript
+
+[<EntryPoint>]
+let main args = 
+    for arg in args do
+        let newArg = PigLatin.toPigLatin arg
+        printfn "%s in Pig Latin is: %s" arg newArg
+    0
